2e800f39de90fb7d8935a5be3482838b68e4c091
Bo Zhang <bzhangusc@live.com>
2018-09-10 16:46:19 -0700

    #2 remove auth from index and notepad

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
   require_once('HTML/Template/ITX.php');
-  require_once ("include/auth.inc");
 
-  $row=checkcookie();
-  
   $template = new HTML_Template_ITX("./template");
 
   $template->loadTemplatefile("skeleton.tpl",true,true);
