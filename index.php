a885815b1ae7e78af73eb20359d3bfa750783da7
Bo Zhang <bzhangusc@live.com>
2018-09-10 15:42:51 -0700

    #2 remove DB dependency from skeleton

--- index.php
+++ index.php
@@ -10,25 +10,5 @@
   $template->addBlockFile("CONTENT_CODE","bookmark","bookmark.tpl");
   $template->touchBlock("bookmark");
 
-  if(isset($row["email"])){
-    $template->addBlockFile("LOGIN_CODE","welcome","welcome.tpl");
-    $template->setVariable("UNAME",$row["email"]);
-  }else{
-    $template->addBlockFile("LOGIN_CODE","login","login.tpl");
-    $template->touchBlock("login");
-  }
-
-  $db_conn = new mysqli("mysql505.ixwebhosting.com","C264588_input",
-                        "Llysc190","C264588_LC");
-  $db_conn->query("SET NAMES gbk");
-  $query="SELECT content,DATE(ctime) as date FROM news ORDER BY sticky DESC, ctime DESC LIMIT 10";
-  $result=$db_conn->query($query);
-  for($i=0;$i<$result->num_rows;$i++){
-    $row = @ $result->fetch_assoc();
-    $template->setCurrentBlock("NEWSBOX");
-    $template->setVariable("NEWS_CODE",$row['content']);
-    $template->setVariable("NEWS_DATE",$row['date']);
-    $template->parseCurrentBlock();
-  }
   $template->show();
 ?>
